3478b529f195b78253442c2428692c909a9a5773
WiktorPieklik <wiktor.pieklik@icloud.com>
2020-10-11 18:52:47 +0200

    New model for data being retrieved

---
+++ app/src/main/java/com/example/covivid/Model/WorldCovidReport.java
@@ -0,0 +1,30 @@
+package com.example.covivid.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WorldCovidReport
+{
+    @SerializedName("TotalConfirmed")
+    private int totalConfirmed;
+
+    @SerializedName("TotalDeaths")
+    private int totalDeaths;
+
+    @SerializedName("TotalRecovered")
+    private int totalRecovered;
+
+    public int getTotalConfirmed()
+    {
+        return totalConfirmed;
+    }
+
+    public int getTotalDeaths()
+    {
+        return totalDeaths;
+    }
+
+    public int getTotalRecovered()
+    {
+        return totalRecovered;
+    }
+}
