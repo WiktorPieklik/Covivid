2f15f651f8cfcb0b77b82a1d6c70c1d4e142a802
WiktorPieklik <wiktor.pieklik@icloud.com>
2020-10-11 17:00:05 +0200

    Models for data being retrieved

---
+++ app/src/main/java/com/example/covivid/Model/BaseCovidReport.java
@@ -0,0 +1,64 @@
+package com.example.covivid.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
+public class BaseCovidReport
+{
+    @SerializedName("Country")
+    protected String country;
+
+    @SerializedName("CountryCode")
+    protected String countryCode;
+
+    @SerializedName("Lat")
+    protected float latitude;
+
+    @SerializedName("Lon")
+    protected float longitude;
+
+    @SerializedName("Cases")
+    protected int cases;
+
+    @SerializedName("Status")
+    protected String status;
+
+    @SerializedName("Date")
+    protected Date date;
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public String getCountryCode()
+    {
+        return countryCode;
+    }
+
+    public float getLatitude()
+    {
+        return latitude;
+    }
+
+    public float getLongitude()
+    {
+        return longitude;
+    }
+
+    public int getCases()
+    {
+        return cases;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public Date getDate()
+    {
+        return date;
+    }
+}
